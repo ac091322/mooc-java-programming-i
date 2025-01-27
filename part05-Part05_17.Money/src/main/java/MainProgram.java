4fffef1e6cd7d41839d0435424e33aa70ea2b121
Alan Chang <alanc091322@gmail.com>
2025-01-26 23:42:52 -0800

    part5 - parts 16-17

--- part05-Part05_17.Money/src/main/java/MainProgram.java
+++ part05-Part05_17.Money/src/main/java/MainProgram.java
@@ -3,5 +3,53 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // test your class here
+        plus();
+        lessThan();
+        minus();
+    }
+
+    public static void plus() {
+        Money a = new Money(10, 0);
+        Money b = new Money(5, 0);
+
+        Money c = a.plus(b);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        // a new Money object is created, and is placed at the end of the strand connected to a
+        //  the old 10 euros at the end of the strand disappears and the Java garbage collector takes care of it
+        a = a.plus(c);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+    public static void lessThan() {
+        Money a = new Money(10, 0);
+        Money b = new Money(3, 0);
+        Money c = new Money(5, 0);
+
+        System.out.println(a.lessThan(b));
+        System.out.println(b.lessThan(c));
+    }
+
+    public static void minus() {
+        Money a = new Money(10, 0);
+        Money b = new Money(3, 50);
+
+        Money c = a.minus(b);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        c = c.minus(a);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -7,8 +7,8 @@ public class Money {
     public Money(int euros, int cents) {
 
         if (cents > 99) {
-            euros = euros + cents / 100;
-            cents = cents % 100;
+            euros += cents / 100;
+            cents %= 100;
         }
 
         this.euros = euros;
@@ -23,13 +23,67 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+        int totalEuros = this.euros + addition.euros;
+        int totalCents = this.cents + addition.cents;
+
+        if (totalCents > 99) {
+            totalCents -= 100;
+            totalEuros += 1;
+        }
+
+        return new Money(totalEuros, totalCents);
+    }
+
+    public boolean lessThan(Money compared) {
+        return (100 * this.euros + this.cents) < (100 * compared.euros() + compared.cents());
+    }
+
+    // public Money minus(Money decreaser) {
+    // int totalEuros = this.euros;
+    // int totalCents = this.cents;
+    //
+    // totalCents -= decreaser.cents;
+    //
+    // if (totalCents < 0) {
+    // totalEuros -= 1;  // borrow 1 euro
+    // totalCents += 100;  // add 100 cents to make it non-negative
+    // }
+    //
+    // totalEuros -= decreaser.euros;
+    //
+    // if (totalEuros < 0) {
+    // totalEuros = 0;
+    // totalCents = 0;
+    // }
+    //
+    // return new Money(totalEuros, totalCents);
+    // }
+    public Money minus(Money decreaser) {
+        int totalEuros = this.euros - decreaser.euros;
+        int totalCents = this.cents - decreaser.cents;
+
+        if (totalCents < 0) {
+            totalCents += 100;
+            totalEuros -= 1;
+        }
+
+        // if the value becomes negative, return zero
+        if (totalEuros < 0) {
+            return new Money(0, 0);
+        }
+
+        return new Money(totalEuros, totalCents);
+    }
+
+    @Override
     public String toString() {
         String zero = "";
+
         if (this.cents < 10) {
             zero = "0";
         }
 
         return this.euros + "." + zero + this.cents + "e";
     }
-
 }
