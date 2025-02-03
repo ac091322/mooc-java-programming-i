b1021c93e4371238baba36c4911bed530fe8d63c
Alan Chang <alanc091322@gmail.com>
2025-02-02 19:34:03 -0800

    part7 - parts 02-03

--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -6,8 +6,13 @@ public class LiquidContainers2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        Container container1 = new Container();
+        Container container2 = new Container();
 
         while (true) {
+            System.out.println(String.format("First: %d/100", container1.contains()));
+            System.out.println(String.format("Second: %d/100", container2.contains()));
+
             System.out.print("> ");
 
             String input = scan.nextLine();
@@ -15,7 +20,24 @@ public class LiquidContainers2 {
                 break;
             }
 
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            if (command.equals("add")) {
+                container1.add(amount);
+
+            } else if (command.equals("move")) {
+                if (amount > container1.contains()) {
+                    amount = container1.contains();
+                }
+
+                container1.remove(amount);
+                container2.add(amount);
+
+            } else if (command.equals("remove")) {
+                container2.remove(amount);
+            }
         }
     }
-
 }
--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,9 +1,72 @@
 
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        System.out.println(Arrays.toString(array));
+
+        System.out.println(smallest(array));
+        System.out.println(indexOfSmallest(array));
+        System.out.println(indexOfSmallestFrom(array, 2));
+
+        MainProgram.swap(array, 0, 3);
+        System.out.println(Arrays.toString(array));
+
+        int[] sortedArray = {8, 3, 7, 9, 1, 2, 4};
+        MainProgram.sort(sortedArray);
     }
 
+    public static int smallest(int[] array) {
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i += 1) {
+            int num = array[i];
+            if (num < smallest) {
+                smallest = num;
+            }
+        }
+
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int indexOfSmallest = 0;
+
+        for (int i = 0; i < array.length; i += 1) {
+            if (array[i] < array[indexOfSmallest]) {
+                indexOfSmallest = i;
+            }
+        }
+
+        return indexOfSmallest;
+    }
+
+    public static int indexOfSmallestFrom(int[] array, int startIndex) {
+        int indexOfSmallest = startIndex;
+
+        for (int i = startIndex; i < array.length; i += 1) {
+            if (array[i] < array[indexOfSmallest]) {
+                indexOfSmallest = i;
+            }
+        }
+
+        return indexOfSmallest;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static void sort(int[] sortedArray) {
+        // no need to check the last index, the number at the last index must be the largest because everything before it is already sorted
+        for (int i = 0; i < sortedArray.length - 1; i += 1) {
+            int smallestIndex = indexOfSmallestFrom(sortedArray, i);
+            swap(sortedArray, i, smallestIndex);
+            System.out.println(Arrays.toString(sortedArray));
+        }
+    }
 }
